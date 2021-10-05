1f3353839ccf9e525665ddffea569d22965e6c26
Benjamin <eunvanz@naver.com>
2021-10-05 22:12:13 +0900

    feat: add index.ts

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import SwipeableViews from "./SwipeableViews";
+
+export { SwipeableView } from "./SwipeableViews";
+export type { SwipeableViewProps, SwipeableViewsProps } from "./SwipeableViews";
+export default SwipeableViews;
